1830ad86c8fa62f42dc6699f417658a131fb62d6
Gearhead-Ayush <ayushbansal1011@gmail.com>
2024-02-28 23:27:52 +0530

    week 2 task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
 import ExpenseForm from './Components/ExpenseForm';
 import ExpenseList from './Components/ExpenseList';
+
+
 import './App.css';
 import { useState } from 'react';
 
 
+
 function App() {
   const [formDataArray, setformDataArray] = useState([])
-  const [total, settotal] = useState([0])
+  const [total, settotal] = useState(0)
+  const [totalCredit, settotalCredit] = useState(0)
+  const [totalDebit, settotalDebit] = useState(0)
+  const [chart, setchart] = useState([{ name: "Income", value: 0, color: "#FFA600", percentage: 0 }, { name: "Grocery", value: 0, color: "#FF6361", percentage: 0 }, { name: "Shopping", value: 0, color: "#58508D", percentage: 0 }, { name: "Travel", value: 0, color: "#BC5090", percentage: 0 }, {
+    name: "Rent or Bills", value: 0, color: "#003F5C", percentage:
+      0
+  }, { name: "Miscellaneous", value: 0, color: "#77DD77", percentage: 0 }])
+
+  const handleDelete = (index) => {
+    const deletedItem = formDataArray[index];
+    const updatedFormDataArray = [...formDataArray];
+    updatedFormDataArray.splice(index, 1);
+    setformDataArray(updatedFormDataArray);
+    
+    
+    deletedItem.ExpenseType === "credit" ?(
+      <>
+        {settotal(total - parseFloat(deletedItem.Amount))}
+        {settotalCredit(totalCredit - parseFloat(deletedItem.Amount))}
+      </>
+    ):
+    (
+      <>
+        {settotal(total + parseFloat(deletedItem.Amount))}
+        {settotalDebit(totalDebit - parseFloat(deletedItem.Amount))}
+      </>
+    )
+    const updatedChart = chart.map(item => {
+      if (item.name === deletedItem.Category) {
+        return { ...item, value: item.value-parseFloat(deletedItem.Amount)  , percentage: ((item.value-parseFloat(deletedItem.Amount)) / (totalCredit + totalDebit)) * 100 };
+      }
+      return item;
+    });
+    const updateChart = [...updatedChart].sort((a, b) => a.value - b.value)
+    setchart(updateChart);
+    
+  };
+
+
   const handleFormSubmit = (data) => {
     setformDataArray([...formDataArray, data])
-    {data.ExpenseType === "credit" ? (settotal([...total, parseFloat(data.Amount) + parseFloat(total[total.length - 1])])) : (settotal([...total, parseFloat(total[total.length - 1]) - parseFloat(data.Amount)]))}
+    {
+      data.ExpenseType === "credit" ? (
+        <>
+          {settotal(total + parseFloat(data.Amount))}
+          {settotalCredit(totalCredit + parseFloat(data.Amount))}
+        </>
+      ) :
+        (
+          <>
+            {settotal(total - parseFloat(data.Amount))}
+            {settotalDebit(totalDebit + parseFloat(data.Amount))}
+          </>
+        )
+
+
+      const updatedChart = chart.map(item => {
+        if (item.name === data.Category) {
+          return { ...item, value: parseFloat(data.Amount) + item.value, percentage: ((parseFloat(data.Amount) + item.value) / (totalCredit + totalDebit)) * 100 };
+        }
+        return item;
+      });
+      const updateChart = [...updatedChart].sort((a, b) => a.value - b.value)
+      setchart(updateChart);
+
+
+    }
+
   }
+
+
+
   return (
     <div className="main">
       <ExpenseForm onSubmit={handleFormSubmit} />
-      <ExpenseList formDataArray={formDataArray} total={total} />
+      <ExpenseList Delete={handleDelete} formDataArray={formDataArray} total={total} totalCredit={totalCredit} totalDebit={totalDebit} chart={chart} />
     </div>
-
-
   );
 }
 
--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,44 +1,59 @@
-import { useForm } from "react-hook-form"
 import React from 'react'
+import { useState } from 'react'
 
-const ExpenseForm = ({onSubmit}) => {
+const ExpenseForm = ({ onSubmit }) => {
+  const [data, setdata] = useState({ ExpenseName: "", ExpenseType: "credit", Category: "Income", Amount: "" })
 
-  const {
-    register,
-    handleSubmit,
-  } = useForm()
 
-  const submitForm = (data) => {
-    
-    onSubmit(data)
+
+  const handleChange = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value })
   }
 
-  return (
-    <div className="container1">
-      <h1>Expenses</h1>
-      <form className="flex" action="" onSubmit={handleSubmit(submitForm)}>
+  const submitForm = (e) => {
+    e.preventDefault();
+    onSubmit(data)
+    setdata({ ExpenseName: "", ExpenseType: "credit", Category: "Income", Amount: "" })
 
-        <label> Expense Name</label>
-        <input  {...register("ExpenseName")} type="text" />
-        <label>Expense Type</label>
+  };
 
 
-        <select  {...register("ExpenseType")} >
-          <option value="credit">CREDIT</option>
-          <option value="debit">DEBIT</option>
-        </select>
+return (
+  <div className="container1">
+    <h2>Transaction Form</h2>
+    <br />
+    <form className="flex" action="" onSubmit={submitForm}>
 
-        <label>Category</label>
-        <input {...register("Category")} type="text" />
+      <label> Expense Name</label>
+      <input type="text" name='ExpenseName' value={data.ExpenseName} onChange={handleChange} />
 
 
-        <label>Amount</label>
-        <input {...register("Amount")} type="text" />
+      <label>Expense Type</label>
+      <select name='ExpenseType' value={data.ExpenseType} onChange={handleChange} >
+        <option value="credit">CREDIT</option>
+        <option value="debit">DEBIT</option>
+      </select>
 
-        <input type="submit" value="submit" />
-      </form>
-    </div>
-  )
+
+      <label>Category</label>
+      <select name='Category' value={data.Category} onChange={handleChange} >
+        <option value="Income">Income</option>
+        <option value="Grocery">Grocery</option>
+        <option value="Shopping">Shopping</option>
+        <option value="Travel">Travel</option>
+        <option value="Rent or Bills">Rent/Bills</option>
+        <option value="Miscellaneous">Miscellaneous</option>
+      </select>
+
+
+
+      <label>Amount</label>
+      <input type="text" name='Amount' value={data.Amount} onChange={handleChange} />
+
+      <input type="submit" value="Make Transaction"  />
+    </form>
+  </div>
+)
 }
 
 export default ExpenseForm
